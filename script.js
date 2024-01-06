100e427d34fd7a9cd662c4dc6f3d1b1aca627c01
Seid Redi <seidredi7@gmail.com>
2024-01-06 09:21:19 -0600

    calculated monthly cost and display on table

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log('script.js file');
 document.addEventListener('DOMContentLoaded',onReady,false);
 
 let employees = [];
+let totalMonthlyCost = 0;
 //add event listener to form
 function onReady() {
     console.log('DOM is ready');
@@ -10,9 +11,9 @@ function onReady() {
 }
 
 //triggered on submission of form
-//collects input values
-//creates an object for each employee
+//collects input values, creates an object for each employee
 //store each employee in an array
+//update dom with new employee info
 function onSubmit(event) {
     event.preventDefault();
     // console.log(event.target.children);
@@ -20,20 +21,40 @@ function onSubmit(event) {
         lastName: event.target.children[1].value,
         id: event.target.children[2].value,
         title: event.target.children[3].value,
-        title: event.target.children[4].value,
+        annualSalary: event.target.children[4].value,
     };
 
+    //store employee in array
     employees.push(emp);
-    console.log(employees);
+    // totalMonthlyCost += emp.annualSalary/12;
+    console.log(totalMonthlyCost);
+
+    //update table with new employee info
+    const tBody = document.getElementById('tBody');
+    tBody.innerHTML += `
+    <tr>
+        <td>${emp.firstName}</td>
+        <td>${emp.lastName}</td>
+        <td>${emp.id}</td>
+        <td>${emp.title}</td>
+        <td>$${emp.annualSalary}</td>
+        <td><button>Delete</button></td>
+    </tr>`
+
     
+    if (employees.length != 0) {
+        const tFoot = document.getElementById('tFoot');
+        tFoot.innerHTML = `<tr><th>Total Monthly Cost : </th><td>$${parseInt(totalMonthlyCost += emp.annualSalary/12)}</td></tr>`
+    }
+
+    //todo: add the total monthly cost
+
+    console.log(tBody);
+
+    //clear inputs
     event.target.children[0].value = '';
     event.target.children[1].value = '';
     event.target.children[2].value = '';
     event.target.children[3].value = '';
-    event.target.children[4].value = '';
-    // for (let index = 0; index < event.target.children.length-1; index++) {
-    //     const inputElement = (event.target.children)[index];
-    //     console.log(inputElement);
-    // }
-
+    event.target.children[4].value = ''
 }
